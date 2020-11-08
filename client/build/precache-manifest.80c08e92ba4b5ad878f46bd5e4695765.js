bf71769540d975c3126e2278985e518fd5150806
Sanketak08 <sakulkarni0801@gmail.com>
2020-11-08 11:35:58 +0530

    Build folder created

---
+++ client/build/precache-manifest.80c08e92ba4b5ad878f46bd5e4695765.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56d551276e21074bc4c1bf4b4c7fec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e74a11d7df3d5fa60114",
+    "url": "/static/css/main.a59fb7c2.chunk.css"
+  },
+  {
+    "revision": "44f2d610e2fabb524c83",
+    "url": "/static/js/2.aabb3e48.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.aabb3e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74a11d7df3d5fa60114",
+    "url": "/static/js/main.d2143805.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,18 +10,21 @@ connectDB();
 //Init Malware
 app.use(express.json({ extended: false }));
 
-app.get("/",(req,res) => {
-    res.send("API Running")
+//To use build folder as static
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
 //Define Routes
-app.use('/api/users',require('./routes/api/users'));
-app.use('/api/profile',require('./routes/api/profile'));
-app.use('/api/posts',require('./routes/api/posts'));
-app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
