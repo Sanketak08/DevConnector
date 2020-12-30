77449721fb5dfb6910d00b648bc322a91c984314
Sanketak08 <sakulkarni0801@gmail.com>
2020-12-30 15:29:32 +0530

    deployment

--- server.js
+++ server.js
@@ -11,27 +11,23 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 //To use build folder as static
-app.use(express.static(path.join(__dirname, '/client/build')));
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-//Serve Static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//     //Set static folder
-//     app.use(express.static('client/build'));
-
-//     app.get('*', (req, res) =>
-//         res.sendFile(path.resolved(__dirname, 'client', 'build', 'index.html'))
-//     );
-// }
+// app.use(express.static(path.join(__dirname, '/client/build')));
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve Static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolved(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 const PORT = process.env.PORT || 5000;
 
